62f7b2f2f99a404c06e57de02c650a6319c6036f
Alex Wishart <awishart@uk.ibm.com>
2018-04-20 15:41:41 +0100

    Address travis failures

--- testGenerator.sh
+++ testGenerator.sh
@@ -1,7 +1,18 @@
+export projectName="swiftserver-generator-projects"
+export startDir=`pwd`
+mkdir ${projectName}
+cd ${projectName}
+export projectFolder=`pwd`
+echo "Generating project"
+yo swiftserver --init --skip-build
+export SWIFT_SNAPSHOT=swift-4.0.3
 echo "Testing swiftserver generated project"
 if ! ./Package-Builder/build-package.sh -projectDir .
 then
   echo "FAILED"
-  rm -rf $projectFolder
+  cd ${startFolder}
+  rm -rf ${projectFolder}
   exit 1
 fi
+cd ${startFolder}
+rm -rf ${projectFolder}
