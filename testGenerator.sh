dcbb546eb5c73884862a9b3e4715471a89f41950
Alex Wishart <awishart@uk.ibm.com>
2018-04-23 13:34:10 +0100

    Restructuring generator tests in travis

--- testGenerator.sh
+++ testGenerator.sh
@@ -7,13 +7,3 @@ echo "Generating project"
 yo swiftserver --init --skip-build
 echo "Testing swiftserver generated project"
 git clone https://github.com/IBM-Swift/Package-Builder.git
-./Package-Builder/build-package.sh -projectDir ${projectFolder}
-if [ $? -eq 0 ]; then
-    echo "Generated project built successfully"
-    cd ${TRAVIS_BUILD_DIR}
-    rm -rf ${projectFolder}
-else
-    echo "FAILURE: Could not build generated project"
-    cd ${TRAVIS_BUILD_DIR}
-    rm -rf ${projectFolder}
-fi
