1af0b13556078681db347ab36f1039d4dc8a25cf
Alex Wishart <awishart@uk.ibm.com>
2018-05-14 16:45:03 +0100

    Take into account new files from generator

--- updateRepo.sh
+++ updateRepo.sh
@@ -28,6 +28,5 @@ fi
 echo "Project needs to be updated"
 cp -r ${newProject}/${projectName}/. ${currentProject}/${projectName}
 cd ${currentProject}/${projectName}
-git add $(git diff --name-only)
-git commit -m "CRON JOB: Updating generated project"
+git commit -am "CRON JOB: Updating generated project"
 git push origin ${BRANCH}
