9ccf471feadea486abac9e5e656ef48b329d79e8
Alex Wishart <awishart@uk.ibm.com>
2018-05-24 10:13:40 +0100

    Change Kitura version in Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "generator-swiftserver-projects",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-            .package(url: "https:///github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.3.0"))
+            .package(url: "https:///github.com/IBM-Swift/Kitura.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
