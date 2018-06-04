dee8bf6414b593fd28188b687afa93c396e31e5d
Travis CI User <travis@example.org>
2018-06-04 15:40:50 +0000

    CRON JOB: Updating generated project

--- iterative-dev.sh
+++ iterative-dev.sh
@@ -12,10 +12,10 @@
 
 # kills the executable for this project and the lldb debug server (if running)
 function killServerProcesses() {
-    pid="$(pgrep Generator-Swiftserver-Projects)"
+    pid="$(pgrep generator-swiftserver-projects)"
     if [ "$pid" != "" ]
     then
-        echo "killing Generator-Swiftserver-Projects"
+        echo "killing generator-swiftserver-projects"
         kill $pid
     fi
 
@@ -80,13 +80,13 @@ while true; do
             # assumes you are inside of Dockerfile-tools container
             # starts debug server on port 1024
             cd /swift-project/
-            /swift-utils/tools-utils.sh debug Generator-Swiftserver-Projects 1024
+            /swift-utils/tools-utils.sh debug generator-swiftserver-projects 1024
         else
             # assumes you are running from "normal"/host operating system
             swift build
             if [[ $? == 0 ]]; then 
                 # only start server is build was successful (result code 0)
-                ./.build/debug/Generator-Swiftserver-Projects &
+                ./.build/debug/generator-swiftserver-projects &
             fi
         fi
         
