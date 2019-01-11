fff8606ecab31775e65c745abb490bbbf9c15e88
David Dunn <davdunn2@uk.ibm.com>
2019-01-11 11:40:47 +0000

    Fix script issues

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Checking if repo needs to be updated"
 export ORG="IBM-Swift"
 export REPO="generator-swiftserver-projects"
@@ -7,18 +9,18 @@ export projectName="Generator-Swiftserver-Projects"
 
 for BRANCH in $BRANCHES
 do
-  cd ${TRAVIS_BUILD_DIR}
+  cd "${TRAVIS_BUILD_DIR}" || exit
   mkdir current
-  cd current
-  git clone -b ${BRANCH} "https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/${ORG}/${REPO}.git"
-  export currentProject=`pwd`
+  cd current || exit
+  git clone -b "${BRANCH}" "https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/${ORG}/${REPO}.git"
+  currentProject=$(pwd)
 
-  mkdir -p ${TRAVIS_BUILD_DIR}/new/${projectName}
-  cd ${TRAVIS_BUILD_DIR}/new/${projectName}
-  if [[ ${BRANCH} == "init" ]]
+  mkdir -p "${TRAVIS_BUILD_DIR}/new/${projectName}"
+  cd "${TRAVIS_BUILD_DIR}/new/${projectName}" || exit
+  if [[ "${BRANCH}" == "init" ]]
   then
     yo swiftserver --init --skip-build
-  elif [[ $BRANCH == "openAPI" ]]
+  elif [[ "$BRANCH" == "openAPI" ]]
   then
     yo swiftserver --app --spec '{ "appName":"'"$projectName"'", "appType":"scaffold", "appDir":".", "openapi":true, "docker":true, "metrics":true, "healthcheck":true }' --skip-build
   fi
@@ -26,23 +28,26 @@ do
   rm spec.json
   echo "Generate README rtf"
   pandoc README.md -f markdown_github -t rtf -so README.rtf
-  cd ${TRAVIS_BUILD_DIR}/new
-  export newProject=`pwd`
+  cd "${TRAVIS_BUILD_DIR}"/new || exit
+  newProject=$(pwd)
 
-  if diff -x '.git' -r ${currentProject}/${REPO} ${newProject}/${projectName}
+  currentRepo="${currentProject}/${REPO}"
+  newRepo="${newProject}/${projectName}"
+
+  if diff -x '.git' -r "${currentRepo}" "${newRepo}"
   then
     echo "Project does not need to be updated"
-    rm -rf ${currentProject}/${REPO} ${newProject}/${projectName}
+    rm -rf "${currentRepo}" "${newRepo}"
     exit 1
   fi
 
   echo "Project needs to be updated"
-  cp -r ${newProject}/${projectName}/. ${currentProject}/${REPO}
-  cd ${currentProject}/${REPO}
+  cp -r "${newProject}/${projectName}/." "${currentProject}/${REPO}"
+  cd "${currentProject}/${REPO}" || exit
   git add .
   git commit -m "CRON JOB: Updating generated project"
-  git push origin ${BRANCH}
-  cd ${TRAVIS_BUILD_DIR}
+  git push origin "${BRANCH}"
+  cd "${TRAVIS_BUILD_DIR}" || exit
   rm -rf current
   rm -rf new
 done
