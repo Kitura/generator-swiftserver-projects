a6787fcf50d1bc8afe415188ed991066848978d6
David Dunn <davdunn2@uk.ibm.com>
2019-01-21 11:20:57 +0000

    Remove checksum

--- updateRepo.sh
+++ updateRepo.sh
@@ -55,7 +55,7 @@ do
   fi
 
   echo "Project needs to be updated"
-  rsync -av --checksum "${newRepo}/" "${currentRepo}"
+  rsync -av --ignore-times "${newRepo}/" "${currentRepo}"
   cd "${currentRepo}"
   git add -A
   git commit -m "CRON JOB: Updating generated project"
