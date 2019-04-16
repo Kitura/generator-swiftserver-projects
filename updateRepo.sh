142257d9da7702ce7501b51df1c26a0449cf2ef9
David Dunn <dd4dev@gmail.com>
2019-04-16 10:06:24 +0100

    Add nightly cron jobs for openAPI branch (#23)
    * Add nightly chron jobs for openAPI branch
    
    * Fix script issues
    
    * Improve error handling in updateRepo.sh
    
    * Test updated script
    
    * Fix fail function
    
    * Tidy up script
    
    * Test rsync
    
    * Add ignore times flag
    
    * Test checksum
    
    * Remove checksum
    
    * Test size only
    
    * Test ignore existing
    
    * Add slash to currentRepo + ignore timestamp
    
    * fix rsnc command
    
    * Revert to Swift dev ops credentials
    
    * Add log for which branch we are generating
    
    * Add several Travis checks to improve script
    
    * Add debug
    
    * Update script to use TRAVIS_REPO_SLUG

--- updateRepo.sh
+++ updateRepo.sh
@@ -1,34 +1,79 @@
+#!/bin/bash
+set -ex
+
 echo "Checking if repo needs to be updated"
-export ORG="IBM-Swift"
-export REPO="generator-swiftserver-projects"
-export GH_REPO="github.com/${ORG}/${REPO}.git"
-export BRANCH="init"
-export projectName="Generator-Swiftserver-Projects"
 
-cd ${TRAVIS_BUILD_DIR}
-mkdir current
-cd current
-git clone -b ${BRANCH} "https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/${ORG}/${REPO}.git"
-export currentProject=`pwd`
-
-mkdir -p ../new/${projectName}
-cd ../new/${projectName}
-yo swiftserver --init --skip-build
-echo "Generate README rtf"
-pandoc README.md -f markdown_github -t rtf -so README.rtf
-cd ../
-export newProject=`pwd`
-
-if diff -x '.git' -r ${currentProject}/${REPO} ${newProject}/${projectName}
-then
-  echo "Project does not need to be updated"
-  rm -rf ${currentProject}/${REPO} ${newProject}/${projectName}
-  exit 1
+if [[ $TRAVIS == true ]]
+then GH_REPO="github.com/${TRAVIS_REPO_SLUG}.git"
+else 
+  # If running locally set ORG to the Org of your fork.
+  ORG="<my-org>"
+  REPO="generator-swiftserver-projects"
+  GH_REPO="github.com/${ORG}/${REPO}.git"
 fi
 
-echo "Project needs to be updated"
-cp -r ${newProject}/${projectName}/. ${currentProject}/${REPO}
-cd ${currentProject}/${REPO}
-git add .
-git commit -m "CRON JOB: Updating generated project"
-git push origin ${BRANCH}
+echo GH_REPO
+
+BRANCHES="init openAPI"
+projectName="Generator-Swiftserver-Projects"
+
+SUCCESS="" # List of successful updates
+FAIL=""    # List of failed updates
+
+# Builds a list of branchs that failed to update (if any)
+function fail () {
+  FAIL="$FAIL $1" && echo "Failed to push to branch: $BRANCH"
+  return 1
+}
+
+for BRANCH in $BRANCHES
+do
+  echo "Generating project for ${BRANCH}"
+  cd "${TRAVIS_BUILD_DIR}"
+  rm -rf current
+  rm -rf new
+  git clone -b "${BRANCH}" "https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@${GH_REPO}" current
+  currentProject="$(pwd)/current"
+
+  # Need to create a project directory and move into it so we can run the generator.
+  mkdir -p "new/${projectName}" && cd "new/${projectName}"
+  if [[ "${BRANCH}" == "init" ]]
+  then
+    yo swiftserver --init --skip-build
+  elif [[ "$BRANCH" == "openAPI" ]]
+  then
+    yo swiftserver --app --spec '{ "appName":"'"$projectName"'", "appType":"scaffold", "appDir":".", "openapi":true, "docker":true, "metrics":true, "healthcheck":true }' --skip-build
+    rm spec.json
+  else
+    exit
+  fi
+  # Step back into the travis build directory after generator has finished.
+  cd "${TRAVIS_BUILD_DIR}"
+
+  echo "Generate README rtf"
+  pandoc README.md -f markdown_github -t rtf -so README.rtf
+  newProject="$(pwd)/new"
+
+  currentRepo="${currentProject}/${REPO}"
+  newRepo="${newProject}/${projectName}"
+
+  if diff -x '.git' -r "${currentRepo}" "${newRepo}"
+  then
+    echo "Project does not need to be updated"
+    rm -rf "${currentProject}" "${newProject}"
+    continue
+  fi
+
+  echo "Project needs to be updated"
+  rsync -av --ignore-times "${newRepo}/" "${currentRepo}/"
+  cd "${currentRepo}"
+  git add -A
+  git commit -m "CRON JOB: Updating generated project"
+  if $TRAVIS_PULL_REQUEST == false 
+  then git push origin "${BRANCH}" || fail "${BRANCH}" || continue
+  fi
+  SUCCESS="$SUCCESS $BRANCH"
+done
+
+echo Success: $SUCCESS
+echo Failed: $FAIL
