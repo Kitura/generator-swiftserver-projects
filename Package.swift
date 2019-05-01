3974668cff86cede5b9ef4b63635172096f83b5b
David Dunn <dd4dev@gmail.com>
2019-05-01 09:45:59 +0100

    Update placeholders to match other projects (#26)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "generator-swiftserver-projects",
+    name: "Generator-Swiftserver-Projects",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.6.0"),
@@ -13,7 +13,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "generator-swiftserver-projects",
+            name: "Generator-Swiftserver-Projects",
             dependencies: ["Kitura"]),
     ]
 )
